dea6f5f4e157245743f826f7d60f2a877d670bd4
Dell <Dell@192.168.0.17>
2019-09-14 13:19:25 -0400

    First Commit

---
+++ devopsexamples/src/main/java/com/adrim/spark/training/devopsexamples/App.java
@@ -0,0 +1,13 @@
+package com.adrim.spark.training.devopsexamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
